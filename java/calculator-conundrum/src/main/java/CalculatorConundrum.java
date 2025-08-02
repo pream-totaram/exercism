5987621f94c4ee04c49aff6e9a62d255e339cae3
Pream Totaram <pream@preamtotaram.com>
2025-08-01 23:45:48 -0400

    Passing tests

--- java/calculator-conundrum/src/main/java/CalculatorConundrum.java
+++ java/calculator-conundrum/src/main/java/CalculatorConundrum.java
@@ -1,5 +1,27 @@
 class CalculatorConundrum {
     public String calculate(int operand1, int operand2, String operation) {
-        throw new UnsupportedOperationException("Please implement the CalculatorConundrum.calculate() method");
+      int result = 0;
+      switch (operation) {
+        case null:
+          throw new IllegalArgumentException("Operation cannot be null");
+        case "":
+          throw new IllegalArgumentException("Operation cannot be empty");
+        case "+":
+          result = operand1 + operand2;
+          break;
+        case "*":
+          result = operand1 * operand2;
+          break;
+        case "/":
+          try {
+            result = operand1 / operand2;
+          } catch (ArithmeticException e) {
+            throw new IllegalOperationException("Division by zero is not allowed", e);
+          }
+          break;
+        default:
+          throw new IllegalOperationException(String.format("Operation '%s' does not exist", operation));
+      }
+      return String.format("%d %s %d = %d", operand1, operation, operand2, result);
     }
 }
